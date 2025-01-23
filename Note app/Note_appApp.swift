38e0f7ba1b0433957a28efc268b03b2f153ab652
ThibaudTraen <thibaud.traen@jopie.be>
2025-01-23 11:23:52 +0100

    Initial Commit

---
+++ Note app/Note_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Note_appApp.swift
+//  Note app
+//
+//  Created by Thibaud Traen on 23/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct Note_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Note appTests/Note_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  Note_appTests.swift
+//  Note appTests
+//
+//  Created by Thibaud Traen on 23/01/2025.
+//
+
+import Testing
+@testable import Note_app
+
+struct Note_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
